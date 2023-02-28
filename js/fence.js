40207b801c7ed14db9931cf613b5f61b7bbd554f
Yakuzi666 <47276749+Yakuzi666@users.noreply.github.com>
2023-02-28 15:14:46 +0100

    Init

---
+++ js/fence.js
@@ -0,0 +1,135 @@
+let options;
+let currentPos;
+let currentPoint;
+let currentDistance;
+let pointCount = 0;
+let searchRadius = 0.02;
+let hasStarted = false;
+let wakeLock = null;
+let wakeLockSupported = false;
+
+//Array 
+//mit Positions Name / Latitude, Longitude / Video, Bild Link
+const posArray =   
+[
+  {name:"02_Steinbruch_Ort", lat: 48.200161, long: 15.607478, cover:1},
+  {name:"03_Verladestation", lat: 48.200136, long: 15.608068, cover:2},
+  {name:"01_Die_Ortsansaessigen", lat: 48.200234, long: 15.606781, cover:0}
+];
+
+navigator.geolocation.watchPosition(succesCallback, errorCallback, options);
+
+//Get the current position
+function succesCallback(pos)
+{
+  if(hasStarted)
+  {
+    currentPos = pos.coords;
+    SearchTriggerPos();
+    UpdateHtmlText();
+  }
+}
+
+//Called if an error appears
+function errorCallback(error)
+{
+  console.log(error);
+}
+
+options = {
+  enableHighAccuracy: true,
+  timeout: 10000,
+  maximumAge: 0
+};
+
+function SearchTriggerPos()
+{
+  let currentPointTemp;
+
+  //Geht durch das posArray
+  for (let index = 0; index < posArray.length; index++) {
+    const element = posArray[index];
+    
+    //Berechnet die Distanz von der aktuellen Position zum Array Punkt
+    distance = calculateDistance(currentPos.latitude, currentPos.longitude,element.lat, element.long);
+    console.log(distance);
+
+    if(distance <= searchRadius && element.name != currentPoint)
+    {
+      currentPointTemp = element;
+      pointCount++;
+    }
+  }
+
+  if(pointCount < 2 && pointCount != 0 )
+  {
+    currentPoint = currentPointTemp.name;
+    console.log(currentPointTemp);
+    currentDistance = calculateDistance(currentPos.latitude, currentPos.longitude,currentPointTemp.lat, currentPointTemp.long);
+    $.getScript("player.js",loadPosition(currentPoint));
+  } 
+
+  pointCount = 0;
+}
+
+//Aktualisiert den Html Text
+function UpdateHtmlText(){
+      $("#currentLat").text(currentPos.latitude);
+      $("#currentLon").text(currentPos.longitude);
+      $("#accuracyText").text(currentPos.accuracy);
+      $("#distance").text(currentDistance);
+}
+
+// Reused code - copyright Moveable Type Scripts - retrieved May 4, 2010.
+// http://www.movable-type.co.uk/scripts/latlong.html
+// Under Creative Commons License http://creativecommons.org/licenses/by/3.0/
+function calculateDistance(lat1, lon1, lat2, lon2)
+{
+  var R = 6371; // km
+  var dLat = (lat2-lat1).toRad();
+  var dLon = (lon2-lon1).toRad();
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return d;
+}
+
+Number.prototype.toRad = function()
+{
+  return this * Math.PI / 180;
+}
+
+function ToggleDebug() {
+  var x = document.getElementById("debug");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+if('wakeLock' in navigator)
+{
+  console.log("WakeLock Supported.");
+  wakeLockSupported = true;
+  document.getElementById("wakeText").innerHTML = wakeLockSupported;
+}
+else
+{
+  console.log("WakeLock not Supported.");
+  wakeLockSupported = false;
+}
+
+async function acquireLock(){
+  wakeLock = await navigator.wakeLock.request("screen");
+  console.log("WakeLock activated.");
+}
+
+function releaseLock(){
+
+}
+
+
+
---
+++ js/player.js
@@ -0,0 +1,133 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn= document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+// Song titles
+const songs=["00_Welcome","01_Die_Ortsansässigen","02_Steinbruch_Ort","03_Verladestation"];
+
+// Keep track of the songs
+let songIndex = 0;
+
+initApp();
+
+function initApp()
+{
+    title.innerText = "Willkommen bei Spuren lesbar machen. Berühren Sie den Play Button um zu starten.";
+    audio.src = `audio/${songs[0]}.mp3`;
+    cover.src = `img/${songs[0]}.jpg`;
+}
+
+function loadSong(song)
+{
+    title.innerText = song;
+    audio.src = `audio/${song}.mp3`;
+    cover.src = `img/${song}.jpg`;
+}
+
+function loadPosition(song)
+{
+    tempTitle = song.replace(/_/g, " ");
+    console.log(tempTitle);
+    title.innerText = tempTitle.slice(3);
+    audio.src = `audio/${song}.mp3`;
+    cover.src = `img/${song}.jpg`;
+    audio.play();
+}
+
+function playSong()
+{
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
+}
+
+function pauseSong()
+{
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    audio.pause();
+}
+
+function prevSong()
+{
+    songIndex--;
+
+    if(songIndex < 0)
+    {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function nextSong()
+{
+    songIndex++;
+
+    if(songIndex > songs.length - 1)
+    {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function updateProgress(e)
+{
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e)
+{
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+// Event Listeners
+playBtn.addEventListener('click', () =>
+{
+    /*
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying)
+    {
+        pauseSong();
+    }
+    else
+    {
+        playSong();
+    } */
+    playSong();
+    title.innerText = "Bewegen Sie sich durch das Gelände und entdecken Sie dessen Geschichte.";
+    document.getElementById("play").style.display = "none";
+    acquireLock();
+    audio.onended = function() {
+        hasStarted = true;
+    };
+})
+
+// Change Song Events
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
+
+progressContainer.addEventListener('click', setProgress);
+
+//Automaticly continue when audio ended
+//audio.addEventListener('ended', nextSong);
